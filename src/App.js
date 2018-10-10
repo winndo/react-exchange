f108441cb064855017b9da1b8cdda5802b39261a
Herwindo Artono <herwindo.artono@gmail.com>
2018-10-10 17:17:18 +0700

    connect API and get rates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.scss';
+import api from './api';
 import BaseCurrency from './components/BaseCurrency';
 
 function Currencies(props) {
@@ -18,7 +19,7 @@ function Currencies(props) {
 }
 
 function AddCurrency(props) {
-    return(
+    return (
         <div className="add-currency">
             + Add Currency
         </div>
@@ -31,26 +32,64 @@ class App extends Component {
         this.state = {
             baseCode: "USD",
             baseValue: 1,
+            supportedCurrencies: api.currency.supported,
+            listNames: {},
+            date: '',
+            rates: {},
+            loading: true
         };
         this.handleBaseValueChange = this.handleBaseValueChange.bind(this);
     }
-    handleBaseValueChange({baseValue}) {
-        this.setState({ baseValue: baseValue });
+
+    componentDidMount() {
+        this.setState({loading: true});
+        api.currency
+            .listNames()
+            .then(names => {
+                this.setState({
+                    listNames: names
+                });
+                return Promise.resolve();
+            })
+            .then(() => {
+                return api.currency.listRates(
+                    this.state.baseCode,
+                    this.state.supportedCurrencies
+                );
+            })
+            .then(data => {
+                this.setState({
+                    date: data.date,
+                    rates: data.rates,
+                    loading: false
+                });
+                return Promise.resolve();
+            });
     }
+
+    handleBaseValueChange({baseValue}) {
+        this.setState({baseValue: baseValue});
+    }
+
     render() {
         return (
             <div id="App">
                 <header id="app-header">
                     <BaseCurrency
-                        title={'United States Dollar'}
+                        title={this.state.listNames[this.state.baseCode]}
                         code={this.state.baseCode}
-                        date={''}
+                        date={this.state.date}
                         baseValue={this.state.baseValue}
                         handleChange={this.handleBaseValueChange}
                     />
                 </header>
                 <div id="main">
-                    <Currencies/>
+                    {this.state.loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <Currencies/>
+                    )}
+
                 </div>
                 <footer id="app-footer">
                     <AddCurrency/>
